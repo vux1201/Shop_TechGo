2c4f36dbf7079942d1f00d2a4fa30ca69534e07f
huongpx <huong.px01@gmail.com>
2023-04-12 14:06:08 +0700

    temp disable expire jwt token

--- src/utils/helpers.py
+++ src/utils/helpers.py
@@ -8,8 +8,9 @@ from core.config import settings
 
 
 def create_access_token(subject: str | Any):
-    expire = datetime.utcnow() + timedelta(minutes=15)
-    to_encode = {"exp": expire, "sub": str(subject)}
+    # expire = datetime.utcnow() + timedelta(minutes=15)
+    # to_encode = {"exp": expire, "sub": str(subject)}
+    to_encode = {"sub": str(subject)}
     encoded_jwt = jwt.encode(to_encode, key=settings.SECRET_KEY)
     return encoded_jwt
 
